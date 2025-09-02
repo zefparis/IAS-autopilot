b02ed0b4ef99e55745e64ff0634b9f8035e049d3
zefparis <ben.barere@gmail.com>
2025-09-02 08:54:27 +0200

    fix

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 const port = process.env.PORT || 8080;
-const command = `serve public -l ${port}`;
+const command = `npx serve public -l ${port}`;
 
 console.log(`Starting server on port ${port}...`);
 const child = exec(command, { stdio: 'inherit' });
